2c8a4e8ff001d9e1f10c6cdad6e6fe028a1e0508
Nazmo <82146066+UngjaeNa@users.noreply.github.com>
2021-04-19 23:57:54 +0900

    Add files via upload

---
+++ 10818.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, i, max, min;
+	scanf("%d", &n);
+	
+	int a[n];
+	for(i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+	
+	max = a[0];
+	for(i=0; i<n; i++){
+		if(max < a[i]) max = a[i];
+	}
+	
+	min = a[0];
+	for(i=0; i<n; i++){
+		if(min > a[i]) min = a[i];
+	}
+	
+	printf("%d %d", min, max);
+	return 0;
+}
